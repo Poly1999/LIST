f68a6da841ccb0617802f59e90a58c6a368e55a4
Poly1999 <pikalova.developer@gmail.com>
2026-07-19 11:50:40 -0400

    feat: use environment variable for API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:8080/api/tasks';
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/tasks';
 
 export function getTasks(params = {}) {
   const query = new URLSearchParams(params).toString();
